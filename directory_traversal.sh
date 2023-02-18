295c4efe7f5aa70d38db2fe13b6ce525d3f9bfdc
ubuntu20 <ubuntu20@mail.com>
2023-02-18 16:22:08 +0800

    Directory traversal: optimize output format

--- directory_traversal.sh
+++ directory_traversal.sh
@@ -1,15 +1,27 @@
 #! /bin/bash
 
+# printFile(filename)
+function printFile() {
+    # print indent space
+    local cnt=0
+    while [ ${cnt} -lt $2 ]; do
+        echo -n " "
+        cnt=`expr ${cnt} + 1`
+    done
+
+    # print item name
+    # `rev` command will reverse the string
+    echo -n "|---"
+    echo ${item} | rev | cut -d "/" -f 1 | rev
+}
+
 # traverse(path, indent)
 function traverse() {
     for item in $1*; do
-        local cnt=0
-            while [ ${cnt} -lt $2 ]; do
-                echo -n " "
-                cnt=`expr ${cnt} + 1`
-            done
-            echo ${item}
+        # print item name
+        printFile ${item} $2
 
+        # if the item is a directory, call this function recursively
         if [ -d ${item} ]; then
             traverse ${item}"/" `expr $2 + 4`  
         fi
